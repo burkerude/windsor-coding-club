0e750a995d5949a559dcb75c3fe14eafdf79005c
burkerude <burkemr@icloud.com>
2021-01-10 12:18:46 -0800

    added different background images(plural) yes I know i did it a bad way, use jinja 2 variables instead

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -9,16 +10,15 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 #Routes
 @app.route("/")
 def root():
-    return render_template("home.html")
+    return render_template("home.html").replace("background.JPEG", f"backgrounds/{random.randint(0, 3)}.JPEG")
 
 @app.route("/home")
 def home():
     return render_template("home.html")
-
 
 @app.route("/resources")
 def resources():
     return render_template("resources.html")
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', debug=False, port=5000)
+  app.run(host = '0.0.0.0', debug=True, port=5000)
