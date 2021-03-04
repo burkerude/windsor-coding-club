880513e66058cfbabe0bb38c1df395d37fa02bba
Ashley Rudelsheim <49853479+ashrude@users.noreply.github.com>
2021-03-04 09:34:21 -0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -21,4 +21,4 @@ def resources():
     return render_template("resources.html")
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', debug=True, port=5000)
+  app.run(host = '0.0.0.0', debug=False, port=5000)
